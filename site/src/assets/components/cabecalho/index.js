39b1d6d7a320232c3809df51981fe2dbf69af82e
Tadeu Henrique Moura Carvalho <tadeuhmc@hotmail.com>
2022-10-18 16:39:13 -0300

    quadradin quase feito,falta a imagem

---
+++ site/src/assets/components/cabecalho/index.js
@@ -0,0 +1,11 @@
+import './index.scss'
+import { Link } from 'react-router-dom'
+
+export default function Cabecalho(){
+
+    return(
+        <div>
+            <Link></Link>
+        </div>
+    )
+}
--- site/src/pages/quadradin/index.js
+++ site/src/pages/quadradin/index.js
@@ -7,17 +7,29 @@ export default function Quadradin() {
     const [altura, setAltura] = useState(0)
     const [largura, setLargura] = useState(0)
     const [resul, setResul] = useState([])
+    const [resul1, setResul1] = useState([])
+    const [resul2, setResul2] = useState([])
     let valor=[]
+    let dor = []
+    let bro=[]
     
     function Contador () {
         for( let u=0; u < altura; u++){
         for(let i=0; i < largura; i++){
-            valor.push("*   ")
+            valor.push("* ")
+            dor.push("■")
+            bro.push(<img src='../../assets/image/Robert.png' alt='Robert'/>)
+
+
         }
-        valor.split("")
+    valor.push(<br/>)
+    dor.push(<br/>)
+    bro.push(<br/>)
     }
 
     setResul(valor)
+    setResul1(dor)
+    setResul2(bro)
 
 
         
@@ -31,6 +43,8 @@ return(
         <button onClick={Contador}>Conte</button>
         <div  className='diladin'>
             <p>{resul}</p>
+            <p>{resul1}</p>
+            <p>{resul2}</p>
         </div>
     </div>
 )
