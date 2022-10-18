138920e3b980c3d408dd4ccb7153b153487abdc4
Tadeu Henrique Moura Carvalho <tadeuhmc@hotmail.com>
2022-10-18 20:33:10 -0300

    efetuada a repetição

--- site/src/pages/quadradin/index.js
+++ site/src/pages/quadradin/index.js
@@ -7,37 +7,41 @@ export default function Quadradin() {
     const [altura, setAltura] = useState(0)
     const [largura, setLargura] = useState(0)
     const [resul, setResul] = useState([])
+    
     const [resul1, setResul1] = useState([])
     const [resul2, setResul2] = useState([])
     const [resul3, setResul3] = useState([])
+    const [resul4, setResul4] = useState([])
+    
     let valor=[]
     let dor = []
     let bro=[]
     let calor=[]
+    let lol=[]
     
     function Contador () {
         for( let u=0; u < altura; u++){
+        
         for(let i=0; i < largura; i++){
             valor.push("* ")
             dor.push("■ ")
             calor.push("● ")
             bro.push(<img className="foto-do-carinha-que-moro-logo-ali" src='/robert.png' alt='Robert'/>)
-
-
+            lol.push(<img className="foto-do-carinha-que-moro-logo-ali" src='/tadeuhmmmmmmmmmmmm.png' alt='Robert'/>)
         }
+
     valor.push(<br/>)
     dor.push(<br/>)
     bro.push(<br/>)
     calor.push(<br/>)
+    lol.push(<br/>)
     }
 
     setResul(valor)
     setResul1(dor)
     setResul2(bro)
     setResul3(calor)
-
-
-        
+    setResul4(lol)
 }
 
 
@@ -51,6 +55,7 @@ return(
             <p>{resul1}</p>
             <p>{resul2}</p>
             <p>{resul3}</p>
+            <p>{resul4}</p>
         </div>
     </div>
 )
