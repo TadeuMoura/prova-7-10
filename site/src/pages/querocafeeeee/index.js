efd750df955b426d900fab02e1c7bc4eb2344b2d
Tadeu Henrique Moura Carvalho <tadeuhmc@hotmail.com>
2022-10-19 22:13:42 -0300

    tentando o café perfeito

--- site/src/pages/querocafeeeee/index.js
+++ site/src/pages/querocafeeeee/index.js
@@ -8,27 +8,33 @@ export default function Cafe() {
     const [capacidade, setCapacidade] =useState(0)
     const [resul ,setResul] =useState(0)
 
+    
+    
+
     function CalcularCafe() {
+        let cadatoma = estudantes*litros/1000
+        let qntcafe = 0
+        let cafe=0
 
-         let aluninhos = (estudantes*litros)/1000
-        
-         for (let cafe=capacidade; cafe<=aluninhos; cafe+capacidade){
-            let cafecafe = cafe
-       
+        while(cafe<=4){
+        qntcafe=capacidade 
+        cafe=cafe+2
+        }
 
-        setResul(cafecafe) }
-        
+    setResul(qntcafe)
+    console.log(qntcafe)
     }
 
+    
 
     return(
         <div className='div-principal-cafe'>
             <label>Quantos estudantes?</label>
             <input onChange={e=> setEstudantes(e.target.value)}/>
+            <label>Qual a capacidade do bule?</label>
+            <input onChange={e=> setCapacidade(e.target.value)}/>
             <label>Quanto cada um bebe?</label>
             <input onChange={e=> setLitros(e.target.value)}/>
-            <label>Quanto de capacidade?</label>
-            <input onChange={e=> setCapacidade(e.target.value)}/>
             <button onClick={CalcularCafe}>CALCULAR CAFÉ</button>
             <h1>{resul}</h1>
         </div>
